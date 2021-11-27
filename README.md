84f7ca6c360751d086f998af2b31cdae5887730f
Shaurya.kalia <shaurya.kalia@wingify.com>
2021-11-28 00:36:58 +0530

    init strapi

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# metamovers-cms
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32952ef1-0644-47c1-8c6a-77dfea206840'
+};
